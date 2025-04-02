3cb50490b45d3c9ef5651c0c14e6ef9f52583994
aasrithaaa_thumms <145799237+taasritha@users.noreply.github.com>
2025-04-02 23:59:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5500;
 const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
 
 app.use(cors());
